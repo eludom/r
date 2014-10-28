cfd51dfc2aefad8ffc97832bd1656befbcca2445
George Jones <gmj@cert.org>
2014-10-28 06:16:34 -0700

    Renamed about(). Added str() and summary().

--- utils/tellMeAbout.r
+++ utils/tellMeAbout.r
@@ -1,36 +1,51 @@
 # Tell me everything you know about /this/ R object
 #
+# Idea from http://stackoverflow.com/questions/1177926/r-object-identification
+#
 # Example usage:
 #
-# > tellMeAbout("foo")
-#   typeof  character 
-#   class  character 
-#   attributes   
-#   names   
-#   sapply(this,class)  character 
-#   sapply(this,attributes)  NULL
-# 
-# > tellMeAbout(c(1,2,3))
-#   typeof  double 
-#   class  numeric 
-#   attributes   
-#   names   
-#   sapply(this,class)  numeric, numeric, numeric 
-#   sapply(this,attributes)  NULL, NULL, NULL 
-#                                        #
-# > tellMeAbout(someComplexObject)
-#   ...
+# > about("foo")
+#  typeof(this)  character 
+#  class(this)  character 
+#  attributes(this)   
+#  names(this)   
+#  sapply(this,class)  character 
+#  sapply(this,attributes)  NULL 
 #
-# > tellMeAbout(someComplexObject)
-#   ...
+#  str(this)
+# chr "foo"
+#
+#  summary(this)
+#   Length     Class      Mode 
+#        1 character character 
+#
+# > about(c(1,2,3))
+#  typeof(this)  double 
+#  class(this)  numeric 
+#  attributes(this)   
+#  names(this)   
+#  sapply(this,class)  numeric, numeric, numeric 
+#  sapply(this,attributes)  NULL, NULL, NULL 
+#
+#  str(this)
+# num [1:3] 1 2 3
+#
+#  summary(this)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#    1.0     1.5     2.0     2.0     2.5     3.0 
+#
 
-# Idea from http://stackoverflow.com/questions/1177926/r-object-identification
-
-tellMeAbout <- function(this) {
-    cat(paste("  typeof ", toString(typeof(this)), "\n"))
-    cat(paste("  class ", toString(class(this)), "\n"))
-    cat(paste("  attributes ", toString(attributes(this)), "\n"))
-    cat(paste("  names ", toString(names(this)), "\n"))
+about <- function(this) {
+    cat(paste("  typeof(this) ", toString(typeof(this)), "\n"))
+    cat(paste("  class(this) ", toString(class(this)), "\n"))
+    cat(paste("  attributes(this) ", toString(attributes(this)), "\n"))
+    cat(paste("  names(this) ", toString(names(this)), "\n"))
     cat(paste("  sapply(this,class) ", toString(sapply(this, class)), "\n"))
     cat(paste("  sapply(this,attributes) ", toString(sapply(this, attributes)), "\n"))
+    cat("\n")
+    cat("  str(this)\n")
+    str(this)
+    cat("\n")    
+    cat("  summary(this)\n")
+    summary(this)    
 }
